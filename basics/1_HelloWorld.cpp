7e64f85afc50e01db3d1afc10844b3419f11fe49
Himanshu Patil <hipatil1998@gmail.com>
2023-01-07 09:43:57 -0500

    Starting with C++

---
+++ basics/1_HelloWorld.cpp
@@ -0,0 +1,15 @@
+// is used for comments in C++
+#include <iostream>
+
+// # is used for preprocessor statement
+// "include" finds files, take all content in that and place in curent file (simply)
+
+int main()
+//ever program has a main function. This is the entry point to every code.
+{
+    std::cout<<"Hello World!"<<std::endl;
+    //every statement in c++ ends with ";"
+    // code is executed in order, except for control flow statements
+
+    // main function even  though data type is int, no return is required.
+}
